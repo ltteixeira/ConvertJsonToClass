ab93ec1a1529e0b86a731529798c6ed0505cf7ff
Luciano <50630976+ltteixeira@users.noreply.github.com>
2022-06-26 12:38:41 -0300

    Add project

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConvertJsonToClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader(@"C:\Users\Branco\Desktop\inserts\coinmarkt\response-teste.json");
+            string jsonString = r.ReadToEnd();
+            Data dados = JsonConvert.DeserializeObject<Data>(jsonString);
+
+            // não testado
+            //using (StreamWriter file = File.CreateText(@"C:\Users\Branco\Desktop\inserts\coinmarkt\novo.txt"))
+            //{
+            //    JsonSerializer serializer = new JsonSerializer();
+            //    serializer.Serialize(file, dados);
+            //}
+        }
+    }
+}
+class Data
+{
+    public CryptoCurrencyList data { get; set; } = new CryptoCurrencyList();
+}
+
+class CryptoCurrencyList
+{
+    public List<Item> cryptoCurrencyList { get; set; } = new List<Item>();
+}
+
+class Item
+{
+    public int id { get; set; }
+
+    public string name { get; set; }
+
+    public string symbol { get; set; }
+
+    public string slug { get; set; }
+
+    public int cmcRank { get; set; }
+}
